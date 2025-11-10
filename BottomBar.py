2fabe35726759b914da5d3c14dbcf9755f9975dd
kahpaibe <85435720+kahpaibe@users.noreply.github.com>
2025-11-10 17:30:35 +0100

    Initial commit

---
+++ BottomBar.py
@@ -0,0 +1,78 @@
+from typing import Any
+
+class BottomBar:
+    """Use of ANSI escape codes to maintain a fixed-height section at the bottom of the terminal."""
+    def __init__(self, bar_height: int):
+        """Use of ANSI escape codes to maintain a fixed-height section at the bottom of the terminal.
+        
+        Args:
+            bar_height (int): The number of lines for the bottom bar.
+        """        
+        assert bar_height > 0, "bar_height must be positive"
+        self._bar_height = bar_height
+
+    def __enter__(self):
+        self.init()
+        return self
+    
+    def __exit__(self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: Any | None) -> None:
+        self.print_final_line()
+
+    def init(self) -> None:
+        """Initialize the bottom bar by printing empty lines. Should be called once at the start."""
+        for _ in range(self._bar_height - 1):
+            print()
+        print("", end='')  # No new line at the end
+
+    def print_line(self, *content: Any) -> None:
+        """Print a line on the top part."""
+        print(end="\n") # New line at the bottom
+        print(f"\033[{self._bar_height}A", end='')  # Move cursor up
+        print("\033[1L", end='')  # Insert 1 line
+        print(*content, end='')  # Print content
+        print(f"\033[{self._bar_height}B", end='')  # Move cursor down
+        print("\033[E", end='')  # Move cursor to beginning of line
+
+    def print_bar_line(self, y: int, *content: Any) -> None:
+        """Print a line in the print bar at the specified y position of the bar.
+        Args:
+            y (int): The line number in the print bar (0-indexed, from top).
+            *content (Any): The content to print.
+        """
+        assert 0 <= y < self._bar_height, "y must be within the print bar height"
+        if y == self._bar_height - 1: # Last line special case
+            print("\033[2K", end='')  # Clear line
+            print(*content, end='')  # Print content
+            print("\033[E", end='')  # Move cursor to beginning of line
+            return
+        
+        print(f"\033[{self._bar_height - y - 1}A", end='')  # Move cursor up
+        print("\033[2K", end='') # Clear line
+        print(*content, end='') # Print content
+        print(f"\033[{self._bar_height - y - 1}B", end='') # Move cursor down
+        print("\033[E", end='')  # Move cursor to beginning of line
+
+    def print_final_line(self) -> None:
+        """Print a final line below the print bar, should be called when exiting."""
+        print("", end='\n')  # New line at the bottom
+        
+
+    
+if __name__ == '__main__':
+    with BottomBar(bar_height=4) as bar:
+        N = 10
+        for i in range(N//2):
+            bar.print_line(f"Main Line {i+1}")
+
+        bar.print_bar_line(0, "Bar Line 1")
+        bar.print_bar_line(1, "Bar Line 2")
+        bar.print_bar_line(2, "Bar Line 3")
+        bar.print_bar_line(3, "Bar Line 4")
+
+        for i in range(N//2, N):
+            bar.print_line(f"Main Line {i+1}")
+
+        bar.print_bar_line(0, "Bar Line 1 bis")
+        bar.print_bar_line(1, "Bar Line 2 bis")
+        bar.print_bar_line(2, "Bar Line 3 bis")
+        bar.print_bar_line(3, "Bar Line 4 bis")
---
+++ README.md
@@ -0,0 +1,53 @@
+# BottomBar
+
+A simple Python utility to create a persistent zone at the bottom of the terminal for displaying status information, while allowing regular output above it. It makes use of ANSI escape codes to manipulate cursor position and clear lines, should allow widely compatibility across different terminal emulators.
+
+## Requirements
+- Python 3.10+
+
+## Usage
+
+The user should call `bottom_bar.init()` before printing anything and `bottom_bar.print_final_line()` before exiting the program to ensure the terminal state is clean. Using a context manager is recommended to handle this automatically.
+
+Basic Example:
+```python
+from BottomBar import BottomBar
+bar = BottomBar(bar_height=2)
+bar.init() # Initialize the bottom bar at the start
+for i in range(5):
+    bar.print_line(f"Main Line {i+1}")
+
+bar.print_bar_line(0, "Status: Running")
+bar.print_bar_line(1, "Progress: 50%")
+bar.print_final_line() # Clean up the terminal before exiting
+```
+Above example may erase last line if an exception occurs before `print_final_line()` is called. To avoid this, use a context manager or finally block.
+
+Clean Example, no context manager:
+```python
+from BottomBar import BottomBar
+bar = BottomBar(bar_height=3)
+bar.init() # Initialize the bottom bar at the start
+try:
+    for i in range(10):
+        bar.print_line(f"Main Line {i+1}")
+    
+    bar.print_bar_line(0, "Status: Running")
+    bar.print_bar_line(1, "Progress: 50%")
+    bar.print_bar_line(2, "Errors: 0")
+finally:
+    bar.print_final_line() # Ensure terminal is cleaned up
+```
+
+Clean Example, context manager.
+```python
+from BottomBar import BottomBar
+
+with BottomBar(bar_height=3) as bar:
+    for i in range(10):
+        bar.print_line(f"Main Line {i+1}")
+    
+    bar.print_bar_line(0, "Status: Running")
+    bar.print_bar_line(1, "Progress: 50%")
+    bar.print_bar_line(2, "Errors: 0")
+```
